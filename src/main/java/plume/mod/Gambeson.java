3a116919936bc7a3209bd7f2a53247f7304262c0
Plumepackdev <eliott@plumepack.com>
2025-02-13 11:41:00 +0100

    Basic items system

--- src/main/java/plume/mod/Gambeson.java
+++ src/main/java/plume/mod/Gambeson.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import plume.mod.item.ModItems;
 
 public class Gambeson implements ModInitializer {
 	public static final String MOD_ID = "gambeson";
@@ -15,10 +16,6 @@ public class Gambeson implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/plume/mod/item/ModItems.java
@@ -0,0 +1,28 @@
+package plume.mod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import plume.mod.Gambeson;
+
+public class ModItems {
+
+    public static final Item COIN=registerItem("coin",new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Gambeson.MOD_ID,"coin")))));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(Gambeson.MOD_ID,name),item);
+    }
+
+    public static void registerModItems(){
+        Gambeson.LOGGER.info("Registering items for "+Gambeson.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(COIN);
+        });
+    }
+}
