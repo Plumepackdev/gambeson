3e640aff741e3f278ff92d00806407d26ce82022
Plumepackdev <eliott@plumepack.com>
2025-02-13 11:12:03 +0100

    Initial commit

---
+++ src/client/java/plume/mod/GambesonClient.java
@@ -0,0 +1,12 @@
+package plume.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+import java.util.logging.Logger;
+
+public class GambesonClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
